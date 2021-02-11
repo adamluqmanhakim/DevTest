5c6532c5cfc46493d2df3a17dd9a91be29ad65cb
adamluqmanhakim <32079467+adamluqmanhakim@users.noreply.github.com>
2021-02-10 23:58:44 -0500

    Update main.ts

--- question1/main.ts
+++ question1/main.ts
@@ -1,4 +1,15 @@
 export function isValidWalk(walk: string[]) {
+    int NS = 0;
+    int WE = 0;
+    if (walk.length == 10){
+      for (string i in walk) { 
+        if (i == 'n') NS += 1; 
+        if (i == 's') NS -= 1; 
+        if (i == 'e') EW += 1; 
+        if (i == 'w') EW -= 1; 
+      } 
+
+    }
   
 }
 
